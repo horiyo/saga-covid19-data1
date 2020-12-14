f22cd7caeee173b064f32a8b8bb4f87e334915a2
Yoshiaki Hori <hori@hori-s.net>
2020-12-14 12:17:15 +0900

    Update web2json-data.py

--- script/web2json-data.py
+++ script/web2json-data.py
@@ -3,18 +3,24 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import json
+import os
 from datetime import datetime, timedelta, timezone
 JST = timezone(timedelta(hours=+9))
 
 url = 'https://www.pref.saga.lg.jp/kiji00373220/index.html'
 response = request.urlopen(url)
-retrieveAt = str(datetime.now(JST).isoformat(timespec='seconds'))
+retrieveAt = datetime.now(JST)
+retYear = retrieveAt.year
+retMonth = retrieveAt.month
+retDay = retrieveAt.day
+ISOretrieveAt = str(retrieveAt.isoformat(timespec='seconds'))
+
 soup = BeautifulSoup(response, features="html.parser")
 response.close()
 
 d = {}
 d['見出し'] = soup.find('title').get_text(strip=True)
-d['retrievedAt'] = retrieveAt
+d['retrievedAt'] = ISOretrieveAt
 tbls = soup.find_all('table', {'class': '__wys_table'})
 rows = tbls[0].find_all('tr')
 t = []
@@ -34,7 +40,9 @@ t.append([k.get_text(strip=True) for k in rows[0].find_all('th')])
 for e in rows[1:]:
   t.append([k.get_text(strip=True) for k in e.find_all('td')])
 d['PCR等検査陽性者一覧'] = t
-fn = './data/data/{}'.format(retrieveAt)
+storeDir = './data/data/{}/{}/{}'.format(retYear, retMonth, retDay)
+os.makedirs(storeDir, exist_os=True)
+fn = '{}/{}'.format(storeDir, ISOretrieveAt)
 fnl = './data/data/data-latest.json'
 fpout  = open(fn, 'w')
 json.dump(d, fpout, ensure_ascii=False, indent=4)
